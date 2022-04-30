a6081eb8f75511ae86e4dc4fd9daed750794fdbe
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-04-30 04:27:33 -0500

    initial commit using fresh start (will replace original beginning --- redoing project initialization in order to get clean copy with default choices + Java 17, and using pandaplanner as name instead of pets-productivity

---
+++ src/main/java/org/launchcode/pandaplanner/PandaplannerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.pandaplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/pandaplanner/PandaplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.pandaplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
