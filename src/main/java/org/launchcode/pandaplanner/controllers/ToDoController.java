01eafa1205126e5ac92bb71fcfe07e1103180850
SpaceJamTim <timothyalexander95@gmail.com>
2022-05-18 15:06:54 -0500

    Added some things and imports here

--- src/main/java/org/launchcode/pandaplanner/controllers/ToDoController.java
+++ src/main/java/org/launchcode/pandaplanner/controllers/ToDoController.java
@@ -1,6 +1,24 @@
 package org.launchcode.pandaplanner.controllers;
 
+import org.launchcode.pandaplanner.data.PetRepository;
+import org.launchcode.pandaplanner.data.ToDoRepository;
+import org.launchcode.pandaplanner.models.AbstractEntity;
+import org.launchcode.pandaplanner.models.Pet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 8080) //Should go around CORS policy issues
+@RestController
+@RequestMapping("/api/ToDo")
 public class ToDoController {
+    @Autowired
+    ToDoRepository toDoRepository;
+    @PostMapping
+    public AbstractEntity<?> postTodo(@RequestBody ToDo todo)
     //method to call saved todo list
 
     //method save new todo to list
